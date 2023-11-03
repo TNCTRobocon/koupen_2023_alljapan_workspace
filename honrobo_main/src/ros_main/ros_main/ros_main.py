b70b95dad83dbacded9622638b01d5f775c7bbed
kou-pen <kou-pen@outlook.jp>
2023-11-03 16:52:34 +0900

    fix automove

--- honrobo_main/src/ros_main/ros_main/ros_main.py
+++ honrobo_main/src/ros_main/ros_main/ros_main.py
@@ -133,7 +133,7 @@ class RosMain(Node):
             self.move_side_distance = self.recog.calc_side_movement(origin_point, detected_rect_point) * self.MAX_MOVE_AXES 
             
             self.move_front_distance = self.recog.calc_front_movement(detected_rect_point,result)
-            self.move_front_distance = (self.move_front_distance / self.MAX_MOVE_METER) * self.MAX_MOVE_AXES * -1
+            self.move_front_distance = (self.move_front_distance / self.MAX_MOVE_METER) * self.MAX_MOVE_AXES 
             return image, depth, self.move_side_distance, self.move_front_distance
         else :
             print("lost")
