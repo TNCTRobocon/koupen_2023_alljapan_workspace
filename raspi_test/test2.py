5ab328146688a2e837d23052d77eb94b1c07259f
kou-pen <kou-pen@outlook.jp>
2023-11-15 22:05:40 +0900

    update servo

--- raspi_test/test2.py
+++ raspi_test/test2.py
@@ -1,28 +1,20 @@
-import RPi.GPIO as GPIO	
+import pigpio
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(12, GPIO.OUT)
-pwm0 = GPIO.PWM(12, 50)
+pi = pigpio.pi()
 
-try:
-    while True:
-        deg = 0
-        deg2 = 180
-        move_deg1 = (deg * 9.5 / 180 + 2.5)
-        move_deg2 = (deg2 * 9.5 / 180 + 2.5)
-        print(move_deg1)
-        pwm0.start(move_deg1)
-        pwm0.ChangeDutyCycle(move_deg1)
-        time.sleep(5)
-        pwm0.stop()
-        print(move_deg2)
-        pwm0.start(move_deg2)
-        pwm0.ChangeDutyCycle(move_deg2)
-        time.sleep(5)
-        pwm0.stop()
+servo1 = 18
 
-except KeyboardInterrupt:
-    pass
-GPIO.cleanup()
-        
+deg1 = 0
+deg2 = 180
+duty1 = (deg1 * 9.5 / 180 + 2.5) * 10000
+duty2 = (deg2 * 9.5 / 180 + 2.5) * 10000
+
+freq = 50 
+
+while True:
+    time.sleep(3)
+    pi.hardware_PWM(servo1, freq, duty1)
+    time.sleep(3)
+    pi.hardware_PWM(servo1, freq, duty2)
+    
