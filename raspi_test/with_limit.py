4ca9d80f9c2365bdd32285779bb4c0c4b6fd4e72
kohki <kou-pen@outlook.jp>
2023-11-16 00:47:52 +0900

    add rope_pushup prototype

--- raspi_test/with_limit.py
+++ raspi_test/with_limit.py
@@ -6,22 +6,68 @@ pi = pigpio.pi()
 SERVO_PIN_LIST = [12, 13]
 LIMIT_PIN_LIST = [2, 3, 4, 17, 27, 22, 23, 24]
 
+freq = 50
+
 get_pin_list = [0] * 8
+limit_tick_list = [0] * 8
 callback_ob = []
 
+old_tick = 0
+servo_status = 0
+servo_counter = 0
+
 for i in range(8):
-    pi.set_mode(LIMIT_PIN_LIST[i], pigpio.INPUT)
-    pi.set_pull_up_down(LIMIT_PIN_LIST[i], pigpio.PUD_UP)
+	pi.set_mode(LIMIT_PIN_LIST[i], pigpio.INPUT)
+	pi.set_pull_up_down(LIMIT_PIN_LIST[i], pigpio.PUD_UP)
 
 def main():
-    for pin in LIMIT_PIN_LIST:
-        cb = pi.callback(pin, pigpio.FALLING_EDGE, pin_callback)
-        callback_ob.append(cb)
-    while True:
-        pass
+	for pin in LIMIT_PIN_LIST:
+		cb = pi.callback(pin, pigpio.EITHER_EDGE, pin_callback)
+		callback_ob.append(cb)
+	while True:
+		print(get_pin_list)
+		servo_check()
+		time.sleep(0.1)
 
+def reset_servo():
+	move_servo(0)
+
+def set_servo():
+	move_servo(180)
+
+def move_servo(deg):
+	deg1 = int((deg * 9.5 / 180 + 2.5) * 10000)
+	deg2 = int(((180 - deg) * 9.5 / 180 + 2.5) * 10000)
+	pi.hardware_PWM(SERVO_PIN_LIST[0], freq, deg1)
+	pi.hardware_PWM(SERVO_PIN_LIST[1], freq, deg2)
+
+def servo_check():
+	global servo_counter, servo_status
+	print(servo_counter, servo_status)
+	if get_pin_list[0] == 0 and get_pin_list[1] == 0:
+		reset_servo()
+		servo_counter = 0
+		servo_status = 0
+	elif get_pin_list[0] == 1 and get_pin_list[1] == 1:
+		if servo_counter <= 5:
+			set_servo()
+			servo_status = 1
+			servo_counter = 0
+		else:
+			servo_counter += 1
+	elif get_pin_list[0] == 1 or get_pin_list[1] == 1:
+		servo_counter += 1 
+	else:
+		return
 def pin_callback(gpio, level, tick):
-    print(gpio, level, tick)
+	diff = abs(tick - limit_tick_list[LIMIT_PIN_LIST.index(gpio)])
+	if diff > 20000:
+		print(gpio, level, tick)
+		changed_limit = LIMIT_PIN_LIST.index(gpio)
+		get_pin_list[changed_limit] = level
+
+	limit_tick_list[LIMIT_PIN_LIST.index(gpio)] = tick
+
 
 
 main()
