5da40e61a1e9b45f259ac282b8ebac40c3b128d7
kou-pen <kou-pen@outlook.jp>
2023-11-15 22:52:13 +0900

    add limit_tester

---
+++ raspi_test/with_limit.py
@@ -0,0 +1,26 @@
+import pigpio
+import time
+
+pi = pigpio.pi()
+
+SERVO_PIN_LIST = [12, 13]
+LIMIT_PIN_LIST = [2, 3, 4, 17, 27, 22, 23, 24]
+
+get_pin_list = [0] * 8
+callback_ob = []
+
+for i in range(8):
+    pi.set_mode(LIMIT_PIN_LIST[i], pigpio.INPUT)
+
+def main():
+    for pin in LIMIT_PIN_LIST:
+        cb = pi.callback(pin, pigpio.FALLING_EDGE, pin_callback)
+        callback_ob.append(cb)
+    while True:
+        pass
+
+def pin_callback(gpio, level, tick):
+    print(gpio, level, tick)
+
+
+
